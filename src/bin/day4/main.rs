d6111c528732888ce9b35c4d3d21e658c6a309bb
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-12-04 08:40:06 -0800

    Day 4 Part 2

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -4,16 +4,78 @@ fn main() {
     println!("AOC 2023 Day 4");
 
     let contents = fs::read_to_string("src/bin/day4/input.txt").expect("Failed to read file");
-    let lines = contents.split("\n");
-    let mut total: u32 = 0;
-    for line in lines {
-        if line.is_empty() {
-            continue;
-        }
-        let mut card = Card::parse(line);
-        total += card.calculate_points();
-    }
+    let mut pile: CardPile = CardPile::load(&contents);
+    let total: u32 = pile.calculate_total_points();
     println!("Part 1: {}", total);
+    let copies: u32 = pile.calculate_copies();
+    println!("Part 2: {}", copies);
+}
+
+struct CardPile {
+    cards: Vec<Card>,
+    total_copies: Option<u32>,
+}
+
+impl CardPile {
+    fn load(txt: &str) -> CardPile {
+        let mut pile = CardPile::new();
+        let lines = txt.split("\n");
+        for line in lines {
+            if line.is_empty() {
+                continue;
+            }
+            pile.push(Card::parse(line));
+        }
+        return pile;
+    }
+
+    fn new() -> CardPile {
+        return CardPile { cards: vec![], total_copies: None };
+    }
+
+    fn push(&mut self, card: Card) {
+        assert_eq!(self.cards.len() as u32, card.id - 1);
+        self.cards.push(card);
+    }
+
+    fn get_immutable(&self, card_id: u32) -> &Card {
+        return &self.cards[(card_id - 1) as usize];
+    }
+
+    fn get(&mut self, card_id: u32) -> &mut Card {
+        return &mut self.cards[(card_id - 1) as usize];
+    }
+
+    fn calculate_total_points(&mut self) -> u32 {
+        let mut total: u32 = 0;
+        for id in 1u32..=(self.cards.len() as u32) {
+            total += self.get(id).calculate_points();
+        }
+        return total;
+    }
+
+    fn calculate_copies(&mut self) -> u32 {
+        if let Some(copies) = self.total_copies {
+            return copies;
+        }
+
+        self.calculate_total_points();
+        for id in 1u32..=(self.cards.len() as u32) {
+            let current = self.get_immutable(id);
+            let instances = current.instances;
+            for offset in 1..=current.matches {
+                let other: &mut Card = self.get(id + offset);
+                other.instances += instances;
+            }
+        }
+
+        let mut copies: u32 = 0;
+        for id in 1u32..=(self.cards.len() as u32) {
+            copies += self.get_immutable(id).instances;
+        }
+        self.total_copies = Some(copies);
+        return copies;
+    }
 }
 
 #[allow(dead_code)]
@@ -24,6 +86,8 @@ struct Card {
     sorted_winning: Vec<u32>,
     sorted_have: Vec<u32>,
     points: Option<u32>,
+    instances: u32, // >= 1 (original + copies)
+    matches: u32
 }
 
 fn nums_to_vec(nums: &str) -> Vec<u32> {
@@ -48,7 +112,7 @@ impl Card {
         win_sorted.sort_unstable();
         have_sorted.sort_unstable();
 
-        return Card { id, winning: win_vec, have: have_vec, sorted_winning: win_sorted, sorted_have: have_sorted, points: None };
+        return Card { id, winning: win_vec, have: have_vec, sorted_winning: win_sorted, sorted_have: have_sorted, points: None, instances: 1, matches: 0 };
     }
 
     fn calculate_points(&mut self) -> u32 {
@@ -59,6 +123,7 @@ impl Card {
 
         let mut have_idx: usize = 0;
         let mut win_idx: usize = 0;
+        self.matches = 0;
 
         while have_idx < self.sorted_have.len() && win_idx < self.sorted_winning.len() {
             let have = self.sorted_have[have_idx];
@@ -70,6 +135,7 @@ impl Card {
                 } else {
                     points *= 2;
                 }
+                self.matches += 1;
                 have_idx += 1;
                 win_idx += 1;
             } else if have < win {
@@ -92,4 +158,23 @@ fn point_calculation() {
     assert_eq!(1, Card::parse("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83").calculate_points());
     assert_eq!(0, Card::parse("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36").calculate_points());
     assert_eq!(0, Card::parse("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11").calculate_points());
+}
+
+#[test]
+fn instance_stacking() {
+    let mut pile: CardPile = CardPile::load("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+");
+    let total = pile.calculate_copies();
+    assert_eq!(1, pile.get(1).instances);
+    assert_eq!(2, pile.get(2).instances);
+    assert_eq!(4, pile.get(3).instances);
+    assert_eq!(8, pile.get(4).instances);
+    assert_eq!(14, pile.get(5).instances);
+    assert_eq!(1, pile.get(6).instances);
+    assert_eq!(30, total);
 }
