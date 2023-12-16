fe750bb46e474ac81f8d72343577b5e624b8c260
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-12-16 13:04:21 -0800

    Day 12 Part 2(a)
    Recursive solution

--- src/bin/day12/main.rs
+++ src/bin/day12/main.rs
@@ -1,27 +1,29 @@
 use std::{fs, fmt::{Display, Error, Formatter}};
 
+use memoize::memoize;
+
 fn main() {
     println!("AOC 2023 Day 12");
 
     let contents = fs::read_to_string("src/bin/day12/input.txt").expect("Failed to read input");
-    debugging();
+    //debugging();
 
     let records: Vec<Record> = contents.trim().split("\n").map(|s| Record::parse(s)).collect();
-    let sum: usize = records.iter().map(|r| r.arrangements()).sum();
+    let sum: u64 = records.iter().map(|r| r.arrangements()).sum();
     println!("Total arrangement count: {}", sum);
 
-    let sum2: usize = records.iter().map(|r| r.expand().arrangements()).sum();
+    let sum2: u64 = records.iter().map(|r| r.expand().arrangements()).sum();
     println!("Total expanded arrangement count: {}", sum2);
 }
 
-fn debugging() {
+/*fn debugging() {
     let r = Record::parse("?###???????? 3,2,1");
     let descriptions = r.variant_records().filter(|r| r.is_valid());
     println!("{}", r);
     for d in descriptions {
         println!("{}", d);
     }
-}
+}*/
 
 #[derive(PartialEq, Clone, Copy)]
 enum Symbol {
@@ -48,6 +50,11 @@ impl Symbol {
     }
 }
 
+#[allow(dead_code)]
+fn colorize(input: &str, r: u8, g: u8, b: u8) -> String {
+    return "\x1b[38;2;".to_owned()+&r.to_string()+";"+&g.to_string()+";"+&b.to_string()+"m"+input+"\x1b[0m";
+}
+
 struct Record {
     conditions: Vec<Symbol>,
     contiguous_damage: Vec<u64>
@@ -63,16 +70,19 @@ impl Record {
     fn expand(&self) -> Record {
         let mut new_conditions: Vec<Symbol> = vec![];
         let mut new_cont_damage: Vec<u64> = vec![];
-        for _ in 0..5 {
+        for i in 1..=5u8 {
             new_conditions.extend(self.copy_conditions());
+            if i < 5 {
+                new_conditions.push(Symbol::Unknown);
+            }
             new_cont_damage.extend(self.copy_damage());
         }
         return Record { conditions: new_conditions, contiguous_damage: new_cont_damage };
     }
 
-    fn arrangements(&self) -> usize {
+    /*fn arrangements(&self) -> usize {
         return self.variant_records().filter(|r| r.is_valid()).count();
-    }
+    }*/
 
     #[inline]
     fn copy_conditions(&self) -> Vec<Symbol> {
@@ -94,7 +104,125 @@ impl Record {
         return new;
     }
 
-    fn variants(&self) -> Box<dyn Iterator<Item = Vec<Symbol>> + '_> {
+    #[inline]
+    fn clone(&self) -> Record {
+        return Record { conditions: self.copy_conditions(), contiguous_damage: self.copy_damage() };
+    }
+
+    fn arrangements(&self) -> u64 {
+        #[cfg(test)]
+        println!("\n\n\n\n");
+        return self.clone().mut_ok_subvariants(0);
+    }
+
+    fn mut_ok_subvariants(&mut self, depth: usize) -> u64 {
+        #[cfg(test)]
+        let colors = [
+            (255, 0, 0),
+            (255, 127, 0),
+            (255, 255, 0),
+            (0, 255, 0),
+            (0, 0, 255),
+            (75, 0, 130),
+            (148, 0, 211)
+        ];
+        #[cfg(test)]
+        let mut indentation = "".to_owned();
+        #[cfg(test)]
+        for d in 0..depth {
+            let (r, g, b) = colors[d%colors.len()];
+            indentation += &colorize(" |", r, g, b);
+        }
+        #[cfg(test)]
+        println!("{}Checking {}", indentation, self);
+        while self.conditions.len() > 0 && self.conditions[0] == Symbol::Operational { // strip leading ....
+            self.conditions.remove(0);
+        }
+        if self.conditions.len() == 0 {
+            if self.contiguous_damage.len() == 0 {
+                #[cfg(test)]
+                println!("{}> {}", indentation, colorize("ran out simultaneously", 0, 255, 0));
+                return 1;
+            } else {
+                #[cfg(test)]
+                println!("{}> ran out of damaged area", indentation);
+                return 0;
+            }
+        }
+        if self.contiguous_damage.len() == 0 {
+            for s in &self.conditions {
+                if *s == Symbol::Damaged {
+                    return 0;
+                }
+            }
+            #[cfg(test)]
+            println!("{}> {}", indentation, colorize("ran out with '.' padding", 0, 255, 0));
+            return 1;
+        }
+        match self.conditions.remove(0) {
+            Symbol::Operational => panic!("Operational should be stripped"),
+            Symbol::Damaged => {
+                let damage_len = self.contiguous_damage[0] as usize;
+                if self.conditions.len() < damage_len-1 {
+                    #[cfg(test)]
+                    println!("{}> not enough conditions", indentation);
+                    return 0;
+                }
+                // in this case conditions[0..damage_len-1] have to all be
+                // '#' or '?' (coerced to '#'); AKA none of them can be Operational
+                // the -1 is because we pre-remove the leading '#'
+                for idx in 0..damage_len-1 {
+                    if self.conditions[idx] == Symbol::Operational {
+                        #[cfg(test)]
+                        println!("{}> found operational at idx {} in damaged area of length {}, self: {}", indentation, idx, damage_len, self);
+                        return 0;
+                    }
+                }
+                // then, conditions[damage_len] has to either a) not exist or b) equal '.' or '?'
+                // and then check children
+                if self.conditions.len() == damage_len-1 {
+                    if self.contiguous_damage.len() == 1 {
+                        #[cfg(test)]
+                        println!("{}> {}", indentation, colorize("perfect end length", 0, 255, 0));
+                        return 1;
+                    } else {
+                        #[cfg(test)]
+                        println!("{}> not enough space for conditions", indentation);
+                        return 0;
+                    }
+                }
+                if self.conditions[damage_len-1] == Symbol::Damaged {
+                    #[cfg(test)]
+                    println!("{}> damaged at end of desired damage length", indentation);
+                    return 0;
+                }
+                let mut child = Record {
+                    conditions: self.conditions[damage_len..self.conditions.len()].to_vec(),
+                    contiguous_damage: self.contiguous_damage[1..self.contiguous_damage.len()].to_vec()
+                };
+                return child.mut_ok_subvariants(depth+1);
+            },
+            Symbol::Unknown => {
+                let mut rc0: Vec<Symbol> = vec![]; // operational variant, pre-strip
+                let mut rc1: Vec<Symbol> = vec![Symbol::Damaged];
+
+                rc0.extend(self.copy_conditions());
+                rc1.extend(self.copy_conditions());
+
+                #[cfg(test)]
+                println!("{} {}", indentation, colorize(&format!("[ '.{}' ]", Record::variant_to_string(&rc0)), 255, 0, 255));
+                let mut r0 = Record { conditions: rc0, contiguous_damage: self.copy_damage() };
+                let c0 = r0.mut_ok_subvariants(depth+1);
+                #[cfg(test)]
+                println!("{} {}", indentation, colorize(&format!("[ '{}' ]", Record::variant_to_string(&rc1)), 255, 0, 255));
+                let mut r1 = Record { conditions: rc1, contiguous_damage: self.copy_damage() };
+                let c1 = r1.mut_ok_subvariants(depth+1);
+                return c0 + c1;
+            },
+        };
+    }
+
+    /*fn variants(&self) -> Box<dyn Iterator<Item = Vec<Symbol>> + '_> {
         let unknown_count = self.conditions.iter().filter(|s| **s == Symbol::Unknown).count();
         if unknown_count == 0 {
             return Box::new(vec![self.copy_conditions()].into_iter());
@@ -155,7 +283,7 @@ impl Record {
 
     fn variant_records(&self) -> Box<dyn Iterator<Item = Record> + '_> {
         return Box::new(self.variants().map(|v| Record { conditions: v, contiguous_damage: self.copy_damage() }));
-    }
+    }*/
 
     fn variant_to_string(data: &Vec<Symbol>) -> String {
         return String::from_iter(data.iter().map(|s| s.encode()));
@@ -173,10 +301,20 @@ impl Display for Record {
 
 #[test]
 fn arrangement_count() {
+    assert_eq!(1, Record::parse(".##.?#??.#.?# 2,1,1,1").arrangements());
+    assert_eq!(1, Record::parse("#??.### 1,1,3").arrangements());
     assert_eq!(1, Record::parse("???.### 1,1,3").arrangements());
     assert_eq!(4, Record::parse(".??..??...?##. 1,1,3").arrangements());
     assert_eq!(1, Record::parse("?#?#?#?#?#?#?#? 1,3,1,6").arrangements());
     assert_eq!(4, Record::parse("????.######..#####. 1,6,5").arrangements());
+    assert_eq!(10, Record::parse("###???????? 3,2,1").arrangements());
     assert_eq!(10, Record::parse("?###???????? 3,2,1").arrangements());
+}
+
+#[test]
+fn argument_count_expanded() {
+    assert_eq!(16, Record::parse("????.#...#... 4,1,1").expand().arrangements());
+    assert_eq!(16384, Record::parse(".??..??...?##. 1,1,3").expand().arrangements());
+    assert_eq!(1, Record::parse("?#?#?#?#?#?#?#? 1,3,1,6").expand().arrangements());
     assert_eq!(506250, Record::parse("?###???????? 3,2,1").expand().arrangements());
 }
