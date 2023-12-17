6c17c919434cac9d0e7d7a7bd58da625d837e3cf
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-12-17 12:19:05 -0800

    Day 15 Part 2

--- src/bin/day15/main.rs
+++ src/bin/day15/main.rs
@@ -1,4 +1,5 @@
 use std::{fs, env};
+use arr_macro::arr;
 
 fn main() {
     println!("AOC 2023 Day 15");
@@ -11,6 +12,9 @@ fn main() {
     for arg in args {
         println!("Hash of '{}' is {}", arg, hash(&arg));
     }
+
+    let hm: HashMap = HashMap::parse(&contents);
+    println!("Part 2: {}", hm.focusing_power());
 }
 
 fn hash_sum(data: &str) -> usize {
@@ -42,4 +46,88 @@ fn hasher() {
 #[test]
 fn hash_summer() {
     assert_eq!(1320, hash_sum("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"));
+}
+
+enum Instruction {
+    Remove(String),
+    Add(String, u8)
+}
+impl Instruction {
+    fn decode(data: &str) -> Instruction {
+        match data.split_once("=") {
+            Some((label, focal_length_str)) => {
+                return Instruction::Add(label.to_owned(), focal_length_str.parse::<u8>().unwrap())
+            },
+            None => {
+                assert!(data.ends_with("-"));
+                return Instruction::Remove(data.strip_suffix("-").unwrap().to_owned());
+            }
+        }
+    }
+}
+
+struct LensPair {
+    label: String,
+    focal_length: u8
+}
+impl LensPair {
+    fn from(label: String, focal_length: u8) -> LensPair {
+        return LensPair { label, focal_length };
+    }
+}
+
+struct HashMap {
+    backing: [Vec<LensPair>; 256]
+}
+impl HashMap {
+    fn parse(instructions: &str) -> HashMap {
+        let mut hm = HashMap { backing: arr![Vec::new(); 256] };
+        let parsed = instructions.trim().split(",").map(|s| Instruction::decode(s));
+        parsed.for_each(|i| hm.handle(i));
+        return hm;
+    }
+
+    fn handle(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::Remove(label) => self.remove(&label),
+            Instruction::Add(label, focal_length) => self.add(&label, focal_length)
+        };
+    }
+
+    fn remove(&mut self, label: &str) {
+        let bucket_id = hash(label);
+        let bucket = &mut self.backing[bucket_id];
+
+        let idx = bucket.iter().enumerate().filter(|(_, pair)| pair.label == label).map(|(i, _)| i).last();
+        if let Some(i) = idx {
+            bucket.remove(i);
+        }
+    }
+
+    fn add(&mut self, label: &str, focal_length: u8) {
+        let bucket_id = hash(label);
+        let bucket = &mut self.backing[bucket_id];
+
+        let idx = bucket.iter().enumerate().filter(|(_, pair)| pair.label == label).map(|(i, _)| i).last();
+        if let Some(i) = idx { // replace existing
+            bucket[i] = LensPair::from(label.to_owned(), focal_length);
+        } else { // add to end
+            bucket.push(LensPair::from(label.to_owned(), focal_length));
+        }
+    }
+
+    fn focusing_power(&self) -> usize {
+        let mut sum = 0;
+        for (box_id, bucket) in self.backing.iter().enumerate() {
+            for (lens_id, pair) in bucket.iter().enumerate() {
+                sum += (box_id+1) * (lens_id + 1) * (pair.focal_length as usize);
+            }
+        }
+        return sum;
+    }
+}
+
+#[test]
+fn instruction_parsing() {
+    assert_eq!(145, HashMap::parse("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7").focusing_power());
 }
