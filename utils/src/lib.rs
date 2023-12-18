f2b1cdd4c75702d38714de61308563d11b18daa7
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-12-18 09:04:40 -0800

    Dijkstra implementation (untested)

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{collections::{HashSet, HashMap}, hash::Hash};
+
 pub fn colorize(input: &str, r: u8, g: u8, b: u8) -> String {
     return "\x1b[38;2;".to_owned()+&r.to_string()+";"+&g.to_string()+";"+&b.to_string()+"m"+input+"\x1b[0m";
 }
@@ -7,4 +9,79 @@ pub fn highlight(input: &str, actually: bool, r: u8, g: u8, b: u8) -> String {
         return input.to_owned();
     }
     return "\x1b[48;2;".to_owned()+&r.to_string()+";"+&g.to_string()+";"+&b.to_string()+"m"+input+"\x1b[0m";
+}
+
+pub trait DijkstraNode {
+    /// Returns a vector of (node, distance) pairs
+    fn get_connected(&self) -> Vec<(Self, usize)> where Self: Sized;
+}
+
+pub struct DijkstraData<Node> where Node: PartialEq + Eq + Hash + Copy + DijkstraNode {
+    unvisited: HashSet<Node>,
+    visited: HashSet<Node>,
+    best_distance: HashMap<Node, usize>
+}
+impl <Node>DijkstraData<Node> where Node: PartialEq + Eq + Hash + Copy + DijkstraNode {
+    /// note: does NOT add initial to frontier (unvisited nodes)
+    fn new(initial: Node) -> DijkstraData<Node> {
+        let unvisited = HashSet::new();
+        let visited = {
+            let mut visited = HashSet::new();
+            visited.insert(initial);
+            visited
+        };
+        let best_distance = {
+            let mut best_distance = HashMap::new();
+            best_distance.insert(initial, 0);
+            best_distance
+        };
+        return DijkstraData { unvisited, visited, best_distance };
+    }
+
+    fn get_best_unvisited(&self) -> Option<Node> {
+        if self.unvisited.len() == 0 {
+            return None;
+        }
+        let mut best: Option<Node> = None;
+        let mut best_distance = usize::max_value();
+        for node in &self.unvisited {
+            let dist = *self.best_distance.get(node).expect("Missing best distance for unvisited point");
+            //.unwrap_or(&usize::max_value());
+            if dist <= best_distance {
+                best_distance = dist;
+                best = Some(*node);
+            }
+        }
+
+        return best;
+    }
+
+
+    pub fn dijkstra(initial: Node) -> DijkstraData<Node> {
+        let mut data = DijkstraData::new(initial);
+        for (other, distance) in initial.get_connected() {
+            data.best_distance.insert(other, distance);
+            data.unvisited.insert(other);
+        }
+
+        while let Some(cur) = data.get_best_unvisited() {
+            let dist_so_far = *data.best_distance.get(&cur).unwrap();
+            for (other, dist) in cur.get_connected() {
+                if data.visited.contains(&other) {
+                    continue;
+                }
+
+                data.unvisited.insert(other);
+                let best_dist = match data.best_distance.get(&other) {
+                    None => dist_so_far + dist,
+                    Some(&existing) => existing.min(dist_so_far + dist)
+                };
+                data.best_distance.insert(other, best_dist);
+            }
+            data.unvisited.remove(&cur);
+            data.visited.insert(cur);
+        }
+
+        return data;
+    }
 }
