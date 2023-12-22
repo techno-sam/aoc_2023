adecce2c66f5650954e3238c7518bbab1cb551e5
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-12-21 23:27:33 -0800

    Day 21 Part 2

--- src/bin/day21/main.rs
+++ src/bin/day21/main.rs
@@ -1,16 +1,41 @@
 use std::{fs, collections::HashSet};
 
 use char_enum_impl::char_enum;
+use utils::colorize;
 
 fn main() {
+    const SIZE: usize = 131;
+    const HALF_SIZE: usize = 65;
     println!("AOC 2023 Day 21");
 
     let contents = fs::read_to_string("src/bin/day21/input.txt").expect("Failed to read input");
 
     let mut field = Field::load(&contents);
+    println!("Field width: {}, height: {}", field.width, field.height);
+    let field2 = field.clone();
 
     let total = field.flood(64);
     println!("Part 1: {}", total);
+    
+    if true {
+        println!("\n\n Part 2 debugging");
+        for mul in 0..=3 {
+            let mut field3 = field2.clone();
+            let steps = HALF_SIZE + (mul * SIZE);
+            let total = field3.flood(steps);
+            println!("After {} iterations: {}", steps, total);
+        }
+    }
+
+    let x = ((26501365 - HALF_SIZE) / SIZE) + 1;
+    // magically calculated from previous sequence using WA:
+    // https://www.wolframalpha.com/input?i=3726%2C+33086%2C+91672%2C+179484%2C+296522%2C+442786%2C+...
+    fn f(x: usize) -> usize {
+        return (14613 * x * x) + 3592 - (14479*x);
+    }
+    println!("Part 2: {}", f(x));
+    //let part2_debug = field2.flood(65);
+    //println!("Part 2 (debugging): {}", part2_debug);
 }
 
 #[allow(dead_code)]
@@ -47,6 +72,19 @@ impl Tile {
             Tile::Explored => false // this could probably be true, but whatever
         }
     }
+
+    fn print(&self) {
+        let (r, g, b);
+        if let Tile::Explored = self {
+            (r, g, b) = (255, 0, 0);
+        } else if let Tile::Start = self {
+            (r, g, b) = (0, 255, 0);
+        } else {
+            (r, g, b) = (255, 255, 255);
+        }
+        let s = colorize(&format!("{}", self.encode()), r, g, b);
+        print!("{}", s);
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -62,6 +100,7 @@ impl Coord {
     }
 }
 
+#[derive(Clone)]
 struct Field {
     /// [row][column] indexed
     tiles: Vec<Vec<Tile>>,
@@ -110,18 +149,21 @@ impl Field {
                     continue;
                 }
 
-                if coord.row == 0 && o_row == -1 {
+                /*if coord.row == 0 && o_row == -1 {
                     continue;
                 }
 
                 if coord.column == 0 && o_column == -1 {
                     continue;
-                }
+                }*/
+
+                let o_row = o_row + self.height as i64;
+                let o_column = o_column + self.width as i64;
 
                 let other = coord.offset(o_row, o_column);
-                if other.row >= self.height || other.column >= self.width {
+                /*if other.row >= self.height || other.column >= self.width {
                     continue;
-                }
+                }*/
                 out.push(other);
             }
         }
@@ -132,15 +174,22 @@ impl Field {
     fn print(&self) {
         for row in 0..self.height {
             for column in 0..self.width {
-                print!("{}", self.tiles[row][column].encode());
+                self.tiles[row][column].print();
+                //print!("{}", self.tiles[row][column].encode());
             }
             println!("");
         }
     }
 
+    fn get(&self, coord: Coord) -> Tile {
+        let row = coord.row % self.height;
+        let column = coord.column % self.width;
+        return self.tiles[row][column];
+    }
+
     fn flood(&mut self, steps: usize) -> usize {
-        println!("Initial state:");
-        self.print();
+        /*println!("Initial state:");
+        self.print();*/
 
         let mut total_others: HashSet<Coord> = HashSet::new();
 
@@ -154,21 +203,23 @@ impl Field {
                 let other_coords = self.valid_offsets(coord);
 
                 for other in other_coords {
-                    let other_tile = &self.tiles[other.row][other.column];
+                    let other_tile = &self.get(other);
                     if other_tile.can_enter() {
                         if step == steps { // only count last step
                             total_others.insert(other);
                         }
                         next_frontier.insert(other);
-                        restore.push((other, self.tiles[other.row][other.column]));
-                        self.tiles[other.row][other.column] = Tile::Explored;
+                        if other.row < self.height && other.column < self.width {
+                            restore.push((other, self.get(other)));
+                            self.tiles[other.row][other.column] = Tile::Explored;
+                        }
                     }
                 }
             }
             current_frontier = next_frontier;
             next_frontier = HashSet::new();
-            println!("\n\nStep {}", step);
-            self.print();
+            /*println!("\n\nStep {}", step);
+            self.print();*/
             for (to_restore, tile) in restore {
                 self.tiles[to_restore.row][to_restore.column] = tile;
             }
